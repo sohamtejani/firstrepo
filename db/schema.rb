cd864d3a0b71d5cb32fbc66a52ca8765997597d0
sohamtejani <sametejani123@gmail.com>
2020-08-27 09:22:42 +0530

    final commit fot initial demo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_25_124312) do
+ActiveRecord::Schema.define(version: 2020_08_26_124542) do
 
   create_table "cities", force: :cascade do |t|
     t.string "name"
